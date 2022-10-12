0d4f62bc2a18fda552a50d3692d94f1db36f0fff
Matthew Williams <maw01091989@gmail.com>
2022-10-12 16:55:40 -0700

    added for loops to generate values and store them in arrays

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  passwordText.value = password;
+  // passwordText.value = password;
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 }
@@ -16,14 +16,15 @@ generateBtn.addEventListener("click", writePassword);
 function generatePassword() {
   /////////////////////////// WRITE YOUR CODE HERE /////////////////////////
 
-
 //object containing the initial type possibilities
 let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-let specialCharacters = [];
+let alphabetUpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let specialCharacters = ['@', '#', '!', '$', '%', '&', '/', '~'];
 
 let possibleChoices = [];
 let finalPassword = [];
+passwordText = finalPassword.toString();
 
 let generatedPassword = {
   characters: 8,
@@ -41,39 +42,46 @@ generatedPassword.capitol = confirm('Do you want uppercase letters included?');
 generatedPassword.lowercase = confirm('Do you want lowercase letters included?');
 
 //check values entered into generatedPassword
+
+
 if (!generatedPassword.characters >= 8 && !generatedPassword.characters <= 128){
   alert("Please choose a valid number of characters.");
   generatedPassword.characters = prompt("Choose a length for password. \n Between 8 -128 characters.");
 }
 
-if (generatedPassword.number){
-  
-}
-
-if (generatedPassword.specialCharacter){
-  
-}
-
-if (generatedPassword.capitol){
-  
-}
-
-if (generatedPassword.lowercase){
-  
-}
-
 if (!generatedPassword.number || !generatedPassword.specialCharacter || !generatedPassword.capitol || !generatedPassword.lowercase){
-
+  return null;
 }
 
+for (let i = 0; i < 5; i++){
+  if (generatedPassword.number){
+    possibleChoices.push (numbers[Math.floor(Math.random() * numbers.length)]);
+  }
+}
+
+for (let i = 0; i < 5; i++){
+  if (generatedPassword.specialCharacter){
+    possibleChoices.push (specialCharacters[Math.floor(Math.random() * specialCharacters.length)]);
+  }
+}
+for (let i = 0; i < 5; i++){
+  if (generatedPassword.capitol){
+    possibleChoices.push (alphabetUpperCase[Math.floor(Math.random() * alphabetUpperCase.length)]);
+  }
+}
+for (let i = 0; i < 5; i++){
+  if (generatedPassword.lowercase){
+    possibleChoices.push (alphabet[Math.floor(Math.random() * alphabet.length)]);
+  }
+}
 //for loop to randomize each character in the total length of password that was selected
-for (let i = 0; i <= generatedPassword.characters; i++){
+for (let i = 0; i < generatedPassword.characters; i++){
   finalPassword.push (possibleChoices[Math.floor(Math.random () * possibleChoices.length)])
 }
 
 
 //validate the requirements selected are met by generated password.
-
 console.log(generatedPassword);
-
+console.log(possibleChoices);
+console.log(finalPassword);
 }
